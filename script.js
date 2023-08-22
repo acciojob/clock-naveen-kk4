1fbaca53933266dc983b44fc2f54effcc89fc74c
naveen-kk4 <133044450+naveen-kk4@users.noreply.github.com>
2023-08-22 17:18:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const element = document.getElementById("timer");
+function date_time() 
+{
+ var dt = new Date();
+document.getElementById('timer').innerHTML = dt.toLocaleString().toLocaleUpperCase();
+setTimeout(function () { date_time(); }, 500);
+}
+date_time();
